ebed987f92bcb0ecca297551d9fd0a8ae368abf0
achaiah <achaiahmanav@gmail.com>
2022-11-16 23:04:35 +0530

    Added get_slot_machine_spin fucntion

--- main.py
+++ main.py
@@ -1,6 +1,37 @@
+import random
+
 MAX_LINES = 3
 MAX_BET = 100
 MIN_BET = 1
+
+ROWS = 3
+COLS = 3
+
+symbol_count = {
+    "A":2,
+    "B":4,
+    "C":6,
+    "D":8
+}
+
+def get_slot_machine_spin(rows,cols,symbols):
+    all_symbols = []
+    for symbol,symbol_count in symbols.items():
+        for _ in range(symbol_count):
+            all_symbols.append(symbol)
+
+    columms = []
+    for _ in range(cols):
+        column = []
+        current_symbols = all_symbols[:]
+        for _ in range(rows):
+            value = random.choice(current_symbols)
+            current_symbols.remove(value)
+            column.append(value)
+
+        columms.append(column)
+    return columms
+
 
 def deposit():
     while True:
@@ -38,7 +69,7 @@ def get_bet():
             if MIN_BET <= amount <= MAX_BET:
                 break
             else:
-                print(f"Amount must be between {MIN_BET} - {MAX_BET}.")
+                print(f"Amount must be between ${MIN_BET} - ${MAX_BET}.")
         else:
             print("Please enter a number.")
 
@@ -54,7 +85,6 @@ def main():
             print(f"You do not have enough to bet that amount, your current balance is {balance}")
         else:
             break
-    print(f"You are betting ${bet} on {lines}. Total bet is equal to ${total_bet}")
-    print(balance,lines)
+    print(f"You are betting ${bet} on {lines} lines. Total bet is equal to ${total_bet}")
 
 main()
